911ceb5808c31247946a6b3b2b5cc10a44c8e720
sandeep <itssandeepsv@gmail.com>
2023-11-28 09:19:58 -0500

    added all the functionality in employee class

--- Employee.java
+++ Employee.java
@@ -1,34 +1,98 @@
-import java.time.Year;
 
-public class Employee {
+import java.time.*;
+public abstract class Employee {
     private final String name;
     private int birth_year;
     private double monthly_income;
     private double occupation_rate;
+    private Vehicle vehicle;
+    private Contract contract;
 
-    public Employee(String name,int birth_year, double occupation_rate) {
-        this.name = name;
-        this.birth_year = birth_year;
-        if (occupation_rate > 100) {
-            this.occupation_rate = 100;
-        } else if (occupation_rate < 10) {
-            this.occupation_rate = 10;
-        } else {
-            this.occupation_rate = occupation_rate;
-        }
-        display();
-    }
-    public Employee(String name,int birth_year){
-        this.name=name;
-        this.birth_year=birth_year;
-        this.occupation_rate=100;
-        display();
-    }
+    static final int GAIN_FACTOR_CLIENT = 500;
+    static final int GAIN_FACTOR_TRAVEL = 100;
+    static final int GAIN_FACTOR_ERROR = 10;
+    static final int GAIN_FACTOR_PROJECTS = 200;
+
+
 
     public int age(){
         int present_year = Year.now().getValue();
         return (present_year-birth_year);
     }
+
+
+    public Employee(String name,int birth_year){
+        this.name=name;
+        this.birth_year=birth_year;
+        this.occupation_rate=100;
+        this.vehicle=null;
+        display();
+    }
+    public Employee(String name,int birth_year,Vehicle vehicle){
+        this.name=name;
+        this.birth_year=birth_year;
+        this.occupation_rate=100;
+        this.vehicle=vehicle;
+        display();
+    }
+    public Employee(String name,int birth_year, double occupation_rate){
+        this.name=name;
+        this.birth_year=birth_year;
+        if (occupation_rate>100){
+            this.occupation_rate=100;
+        } else if (occupation_rate < 10) {
+            this.occupation_rate=10;
+        }
+        else {
+            this.occupation_rate=occupation_rate;
+        }
+        this.vehicle=null;
+        display();
+
+    }
+    public Employee(String name,int birth_year, double occupation_rate,Vehicle vehicle){
+        this.name=name;
+        this.birth_year=birth_year;
+        if (occupation_rate>100){
+            this.occupation_rate=100;
+        } else if (occupation_rate < 10) {
+            this.occupation_rate=10;
+        }
+        else {
+            this.occupation_rate=occupation_rate;
+        }
+        this.vehicle=vehicle;
+        display();
+
+    }
+    public void empinfo(){
+        System.out.println("-------------------------");
+        System.out.println("Name: "+getName()+", a "+getClass().getSimpleName());
+        System.out.println("Age:"+age());
+        System.out.println("Employee Has a "+getVehicle().getClass().getSimpleName());
+        System.out.println("\t- make: "+getVehicle().getMake());
+        System.out.println("\t- plate: "+getVehicle().getPlate());
+        System.out.println("\t- color: "+getVehicle().getColor());
+        System.out.println("\t- category: "+getVehicle().getCategory());
+        if (getVehicle().getClass().getSimpleName().equals("Car")){
+            Car c = (Car)getVehicle();
+            System.out.println("\t- gear type: "+c.getGear());
+            System.out.println("\t- type: "+c.getType());
+        }
+        else{
+            Motorcycle m = (Motorcycle)getVehicle();
+            if(m.getSidecar()){
+                System.out.println("\t- with sidecar");
+            }
+            else{
+                System.out.println("\t- without sidecar");
+            }
+        }
+    }
+    public void display(){
+        System.out.println("We have a new employee:"+this.getName()+", a "+this.getClass().getSimpleName());
+    }
+
     public double getMonthly_income() {
         return monthly_income;
     }
@@ -36,6 +100,9 @@ public class Employee {
     public String getName() {
         return name;
     }
+
+
+
     public void setOccupation_rate(double occupation_rate) {
         if (occupation_rate>100){
             this.occupation_rate=100;
@@ -46,6 +113,7 @@ public class Employee {
             this.occupation_rate=occupation_rate;
         }
     }
+
     public double getOccupation_rate() {
         return occupation_rate;
     }
@@ -57,22 +125,52 @@ public class Employee {
     public void setBirth_year(int birth_year) {
         this.birth_year = birth_year;
     }
-    public void display(){
-        System.out.println("We have a new employee:"+this.getName()+", a "+this.getClass().getSimpleName());
-    }
-}
 
+    public void setVehicle(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    public abstract void getEmployeeInfo();
+
+    public void signContract(Contract contract){
+
+        this.contract=contract;
+        this.monthly_income=contract.getMonthlyIncome();
+    }
+
+    public Contract getContract() {
+        return contract;
+    }
+
+    public abstract void contractInfo();
+
+
+}
 class Manager extends Employee{
     private int num_of_travelled_days;
     private  int num_of_clients;
 
+    public Manager(String name,int birth_year,int num_of_clients,int num_of_travelled_days){
+        super(name,birth_year);
+        this.num_of_travelled_days=num_of_travelled_days;
+        this.num_of_clients=num_of_clients;
+    }
+    public Manager(String name,int birth_year,int num_of_clients,int num_of_travelled_days,Vehicle vehicle){
+        super(name,birth_year,vehicle);
+        this.num_of_travelled_days=num_of_travelled_days;
+        this.num_of_clients=num_of_clients;
+    }
     public Manager(String name,int birth_year,int num_of_clients,int num_of_travelled_days,double occupation_rate){
         super(name,birth_year,occupation_rate);
         this.num_of_travelled_days=num_of_travelled_days;
         this.num_of_clients=num_of_clients;
     }
-    public Manager(String name,int birth_year,int num_of_clients,int num_of_travelled_days){
-        super(name,birth_year);
+    public Manager(String name,int birth_year,int num_of_clients,int num_of_travelled_days,double occupation_rate,Vehicle vehicle){
+        super(name,birth_year,occupation_rate,vehicle);
         this.num_of_travelled_days=num_of_travelled_days;
         this.num_of_clients=num_of_clients;
     }
@@ -92,15 +190,60 @@ class Manager extends Employee{
     public int getNum_of_travelled_days() {
         return this.num_of_travelled_days;
     }
+    public double annualIncome(){
+        return (((GAIN_FACTOR_TRAVEL*getNum_of_travelled_days())+(GAIN_FACTOR_CLIENT*getNum_of_clients()))+(getOccupation_rate()/100)
+                *getMonthly_income()*12);
+    }
+    public void getEmployeeInfo(){
+        empinfo();
+        System.out.println(getName()+" has an Occupation rate: "+getOccupation_rate()+"% He/She travelled "
+                +getNum_of_travelled_days()+" days and has brought "+getNum_of_clients()+" new clients.");
+        System.out.println("His/Her Estimated annual income is: "+annualIncome());
+    }
 
+    @Override
+    public void contractInfo() {
+        if(getContract().getClass().getSimpleName().equals("Permanent")){
+            Permanent p = (Permanent)getContract();
+            String cs;
+            if (p.get_civil_status()){
+                cs="married";
+            }
+            else{
+                cs="not married";
+            }
+            System.out.println(getName()+" is a "+getClass().getSimpleName()+". he/she is "+cs+" and he/she has "
+                    +p.getNo_of_children()+" children");
+            System.out.println("He/She has worked for "+p.getDays_worked()+" days and upon contract his/her monthly salary is "
+                    +p.getMonthly_salary());
+        }
+        else{
+            Temporary t = (Temporary) getContract();
+            System.out.println(getName()+" is a "+getClass().getSimpleName()+". he/she is a "
+                    +t.getClass().getSimpleName()+" employee with "+t.getHourly_wage()
+                    +" hourly salary and he has worked for "+t.getHours_worked()+" hours");
+        }
+
+    }
 }
 
 class Tester extends Employee{
     private int num_of_bugs;
+
+
+    public Tester(String name, int birth_year,int num_of_bugs,Vehicle vehicle){
+        super(name, birth_year,vehicle);
+        this.num_of_bugs=num_of_bugs;
+    }
     public Tester(String name, int birth_year, int num_of_bugs,double occupation_rate) {
         super(name, birth_year,occupation_rate);
         this.num_of_bugs=num_of_bugs;
     }
+    public Tester(String name, int birth_year,int num_of_bugs,double occupation_rate,Vehicle vehicle) {
+        super(name, birth_year,occupation_rate,vehicle);
+        this.num_of_bugs=num_of_bugs;
+    }
+
     public void setNum_of_bugs(int num_of_bugs) {
         this.num_of_bugs = num_of_bugs;
     }
@@ -108,22 +251,100 @@ class Tester extends Employee{
     public int getNum_of_bugs() {
         return num_of_bugs;
     }
+
+    public double annualIncome(){
+        return (((GAIN_FACTOR_ERROR*num_of_bugs))+(getOccupation_rate()/100)*getMonthly_income()*12);
+
+    }
+    public void getEmployeeInfo(){
+        empinfo();
+        System.out.println(getName()+" has an Occupation rate: "+getOccupation_rate()+" and corrected "
+                +getNum_of_bugs()+" bugs");
+        System.out.println("His/Her Estimated annual income is: "+annualIncome());
+    }
+    public void contractInfo() {
+        if(getContract().getClass().getSimpleName().equals("Permanent")){
+            Permanent p = (Permanent)getContract();
+            String cs;
+            if (p.get_civil_status()){
+                cs="married";
+            }
+            else{
+                cs="not married";
+            }
+            System.out.println(getName()+" is a "+getClass().getSimpleName()+". he/she is "+cs+" and he/she has "
+                    +p.getNo_of_children()+" children");
+            System.out.println("He/She has worked for "+p.getDays_worked()+" days and upon contract his/her monthly salary is "
+                    +p.getMonthly_salary());
+        }
+        else{
+            Temporary t = (Temporary) getContract();
+            System.out.println(getName()+" is a "+getClass().getSimpleName()+". he/she is a "
+                    +t.getClass().getSimpleName()+" employee with "+t.getHourly_wage()
+                    +" hourly salary and he has worked for "+t.getHours_worked()+" hours");
+        }
+
+    }
 }
 
 class Programmer extends Employee{
     private int num_of_projects;
+
+    public Programmer(String name, int birth_year,int num_of_projects){
+        super(name, birth_year);
+        this.num_of_projects=num_of_projects;
+    }
+    public Programmer(String name, int birth_year,int num_of_projects,Vehicle vehicle){
+        super(name, birth_year,vehicle);
+        this.num_of_projects=num_of_projects;
+    }
     public Programmer(String name, int birth_year, int num_of_projects,double occupation_rate) {
         super(name, birth_year,occupation_rate);
         this.num_of_projects=num_of_projects;
     }
-    public Programmer(String name, int birth_year, int num_of_projects) {
-        super(name, birth_year);
+    public Programmer(String name, int birth_year, int num_of_projects,double occupation_rate,Vehicle vehicle) {
+        super(name, birth_year,occupation_rate,vehicle);
         this.num_of_projects=num_of_projects;
     }
+
     public void setNum_of_projects(int num_of_projects) {
         this.num_of_projects = num_of_projects;
     }
+
     public int getNum_of_projects() {
         return num_of_projects;
+    }
+    public double annualIncome(){
+        return (((GAIN_FACTOR_PROJECTS*num_of_projects))+(getOccupation_rate()/100)*getMonthly_income()*12);
+
+    }
+    public void getEmployeeInfo(){
+        empinfo();
+        System.out.println(getName()+" has an Occupation rate: "+getOccupation_rate()
+                +" and completed "+getNum_of_projects()+" projects");
+        System.out.println("His/Her Estimated annual income is: "+annualIncome());
+    }
+    public void contractInfo() {
+        if(getContract().getClass().getSimpleName().equals("Permanent")){
+            Permanent p = (Permanent)getContract();
+            String cs;
+            if (p.get_civil_status()){
+                cs="married";
+            }
+            else{
+                cs="not married";
+            }
+            System.out.println(getName()+" is a "+getClass().getSimpleName()+". he/she is "+cs+" and he/she has "
+                    +p.getNo_of_children()+" children");
+            System.out.println("He/She has worked for "+p.getDays_worked()+" days and upon contract his/her monthly salary is "
+                    +p.getMonthly_salary());
+        }
+        else{
+            Temporary t = (Temporary) getContract();
+            System.out.println(getName()+" is a "+getClass().getSimpleName()+". he/she is a "
+                    +t.getClass().getSimpleName()+" employee with "+t.getHourly_wage()
+                    +" hourly salary and he has worked for "+t.getHours_worked()+" hours");
+        }
+
     }
 }
